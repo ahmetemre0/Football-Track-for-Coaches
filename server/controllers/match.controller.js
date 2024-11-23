27dbe3e3a8b1de8087b2f476924057c4c2212382
ahmetemre0 <buz20@itu.edu.tr>
2024-11-23 16:38:03 +0300

    add getMatches method to retrieve matches by ID or all matches

--- server/controllers/match.controller.js
+++ server/controllers/match.controller.js
@@ -1,1 +1,11 @@
 const Match = require('../handlers/match.handler');
+
+exports.getMatches = (req, res) => {
+    if (req.query.id) {
+        let match = Match.getByID(req.query.id);
+        res.json({match: match, success: true});
+        return
+    }
+    let matches = Match.getAll();
+    res.json({matches: matches, success: true});
+}
