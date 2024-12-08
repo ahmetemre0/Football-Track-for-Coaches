c8c29abdec92f65c52a013e83997289f2de16754
ahmetemre0 <buz20@itu.edu.tr>
2024-12-08 18:45:48 +0300

    Add routes for retrieving area and no-area actions in action router

--- server/routers/action.router.js
+++ server/routers/action.router.js
@@ -2,6 +2,8 @@ const actionController = require('../controllers/action.controller');
 const router = require('express').Router();
 
 router.get('/', actionController.getAll);
+router.get('/area', actionController.getAreaActions);
+router.get('/no-area', actionController.getNoAreaActions);
 router.get('/:id', actionController.getByID);
 router.post('/', actionController.insert);
 router.put('/:id', actionController.update);
