faad7b8b94054fa9c8295e5d5e3ac7cd489ab2db
ahmetemre0 <buz20@itu.edu.tr>
2024-12-08 19:01:01 +0300

    Add methods for inserting, substituting, and retrieving players in comp handler

--- server/handlers/comp.handler.js
+++ server/handlers/comp.handler.js
@@ -26,3 +26,75 @@ exports.getAll = () => {
         });
     });
 }
+
+exports.insert = async (teamID, matchID, playerID, isFirstEleven, inMatch) => {
+    let query = `INSERT INTO comp (teamID, matchID, playerID, isFirstEleven, inMatch) VALUES (${teamID}, ${matchID}, ${playerID}, ${isFirstEleven}, ${inMatch})`;
+    return new Promise((resolve, reject) => {
+        db.run(query, function (err) {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+            resolve(this.lastID);
+        });
+    });
+}
+
+exports.substitute = async (teamID, matchID, outPlayerID, inPlayerID) => {
+    let outQuery = `UPDATE comp SET inMatch = 0 WHERE teamID = ${teamID} AND matchID = ${matchID} AND playerID = ${outPlayerID}`;
+    let inQuery = `UPDATE comp SET inMatch = 1 WHERE teamID = ${teamID} AND matchID = ${matchID} AND playerID = ${inPlayerID}`;
+    return new Promise((resolve, reject) => {
+        db.run(outQuery, function (err) {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+            db.run(inQuery, function (err) {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+exports.getSquad = (teamID, matchID) => {
+    let query = `SELECT * FROM comp WHERE teamID = ${teamID} AND matchID = ${matchID}`;
+    return new Promise((resolve, reject) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
+
+exports.getFirstEleven = (teamID, matchID) => {
+    let query = `SELECT * FROM comp WHERE teamID = ${teamID} AND matchID = ${matchID} AND isFirstEleven = 1`;
+    return new Promise((resolve, reject) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
+
+exports.getCurrent11 = (teamID, matchID) => {
+    let query = `SELECT * FROM comp WHERE teamID = ${teamID} AND matchID = ${matchID} AND inMatch = 1`;
+    return new Promise((resolve, reject) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
