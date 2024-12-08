66d34d07a6d4840c5f3423d6c04349e29d8d04e1
ahmetemre0 <buz20@itu.edu.tr>
2024-12-08 17:07:27 +0300

    Add CRUD routes for action management in the router

--- server/routers/action.router.js
+++ server/routers/action.router.js
@@ -2,5 +2,9 @@ const actionController = require('../controllers/action.controller');
 const router = require('express').Router();
 
 router.get('/', actionController.getAll);
+router.get('/:id', actionController.getByID);
+router.post('/', actionController.insert);
+router.put('/:id', actionController.update);
+router.delete('/:id', actionController.delete);
 
 module.exports = router;
